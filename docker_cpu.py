e2fafcb99ab2901e8dd62e5778146799318ae300
sunwoo <summerham22@gmail.com>
2024-10-30 11:24:51 +0900

    docker-compose

--- docker_cpu.py
+++ docker_cpu.py
@@ -3,7 +3,7 @@ import json
 import os
 
 # 컨테이너의 CPU 할당량 설정
-container_name = 'blog=1'
+cnt = 2
 
 # Docker stats 명령어 실행
 r = subprocess.check_output(['docker', 'stats', 'ham-blog-1', '--no-stream', '--format', '{{ json . }}'])
@@ -15,12 +15,12 @@ stats = json.loads(r.decode("utf-8"))
 cpu_usage = float(stats["CPUPerc"].strip('%'))  # '%'를 제거하고 float로 변환
 
 while True:
-    if cpu_usage >= 0.01:
+    if cpu_usage <= 0.01:
     #docker compose up -d --scale blog=1
-        subprocess.run(['docker', 'compose', 'up', '-d','--scale', container_name], capture_output=True, text=True)
-        print(f"{container_name}의 CPU 할당되었습니다.")
+        subprocess.run(['docker', 'compose', 'up', '-d','--scale', f'blog={cnt}'], capture_output=True, text=True)
+        print(f"CPU {cnt} 만큼 할당되었습니다.")
     else:
-        print("CPU 사용량이 0.01% 이하입니다.")
+        print("CPU 사용량이 0.01% 미만입니다.")
 
 
 #if r.returncode == 0:
